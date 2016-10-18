5bec5ac8fe31c38e521c244db6e9cb68fe55b295
Ricardo Ferreira da Silva <tansvanio@hotmail.com>
2016-10-18 16:39:49 +0100

    Hello Jade

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,7 @@ import jade.Boot;
 public class Main {
 
 	public static void main(String[] args) {
+		System.out.println("Hello Jade.");
 		String[] param = new String[ 1 ];
 		param[ 0 ] = "-gui";
 		Boot.main( param );
